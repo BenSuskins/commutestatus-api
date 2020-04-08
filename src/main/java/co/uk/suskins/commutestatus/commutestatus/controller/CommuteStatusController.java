ff30bce9e4fe18b8ce6d1e6a5bc295ed1b5df0b5
Ben Suskins <suskinsdevelopment@gmail.com>
2020-04-08 20:16:24 +0100

    removed unncessary imports

--- src/main/java/co/uk/suskins/commutestatus/commutestatus/controller/CommuteStatusController.java
+++ src/main/java/co/uk/suskins/commutestatus/commutestatus/controller/CommuteStatusController.java
@@ -1,7 +1,6 @@
 package co.uk.suskins.commutestatus.commutestatus.controller;
 
 import co.uk.suskins.commutestatus.common.controller.BaseController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
