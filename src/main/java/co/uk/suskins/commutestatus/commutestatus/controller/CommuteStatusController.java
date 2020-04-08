b3374c374bbcf615b62fc095017425e64bc16493
Ben Suskins <suskinsdevelopment@gmail.com>
2020-04-08 20:14:32 +0100

    Added base controller Added swagger and swagger ui Added swagger config Removed double hash in app props

--- src/main/java/co/uk/suskins/commutestatus/commutestatus/controller/CommuteStatusController.java
+++ src/main/java/co/uk/suskins/commutestatus/commutestatus/controller/CommuteStatusController.java
@@ -1,4 +1,10 @@
 package co.uk.suskins.commutestatus.commutestatus.controller;
 
-public class CommuteStatusController {
+import co.uk.suskins.commutestatus.common.controller.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "commutestatus")
+public class CommuteStatusController extends BaseController {
 }
--- src/main/java/co/uk/suskins/commutestatus/user/controller/UserController.java
+++ src/main/java/co/uk/suskins/commutestatus/user/controller/UserController.java
@@ -1,4 +1,10 @@
 package co.uk.suskins.commutestatus.user.controller;
 
-public class UserController {
+import co.uk.suskins.commutestatus.common.controller.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "users")
+public class UserController extends BaseController {
 }
