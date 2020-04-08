417c71869540201f6bc30a36ffe63abf13265290
Ben Suskins <suskinsdevelopment@gmail.com>
2020-04-08 20:15:19 +0100

    Removed class level path as some paths may be secured

--- src/main/java/co/uk/suskins/commutestatus/user/controller/UserController.java
+++ src/main/java/co/uk/suskins/commutestatus/user/controller/UserController.java
@@ -5,6 +5,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "users")
 public class UserController extends BaseController {
 }
