dfdf75fa79413481720ac03c931b034a0b315362
Ben Suskins <suskinsdevelopment@gmail.com>
2020-04-11 12:00:52 +0100

    CMST-6 Ability to call National Rail Darwin - Added WSDL generator code - Added generated code to wsdl - Created controller

--- src/main/java/uk/co/suskins/commutestatus/commutestatus/controller/CommuteStatusController.java
+++ src/main/java/uk/co/suskins/commutestatus/commutestatus/controller/CommuteStatusController.java
@@ -1,8 +1,26 @@
 package uk.co.suskins.commutestatus.commutestatus.controller;
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.co.suskins.commutestatus.common.controller.BaseController;
+import uk.co.suskins.commutestatus.commutestatus.models.api.CommuteStatusResponse;
+import uk.co.suskins.commutestatus.commutestatus.service.CommuteStatusService;
 
 @RestController
 public class CommuteStatusController extends BaseController {
+    private final CommuteStatusService commuteStatusService;
+
+    @Autowired
+    public CommuteStatusController(CommuteStatusService commuteStatusService) {
+        this.commuteStatusService = commuteStatusService;
+    }
+
+    @GetMapping("/secured/commutestatus")
+    @ApiOperation("Returns the calling users commute status")
+    public CommuteStatusResponse postUser() {
+        return null;
+    }
 }
+
