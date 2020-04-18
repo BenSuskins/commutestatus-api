70d0d424cf76bbc67e863694e919d0e8837a7343
Ben Suskins <suskinsdevelopment@gmail.com>
2020-04-18 13:25:56 +0100

    Handling for no trains

--- src/main/java/uk/co/suskins/commutestatus/commutestatus/models/mapper/CommuteStatusMapper.java
+++ src/main/java/uk/co/suskins/commutestatus/commutestatus/models/mapper/CommuteStatusMapper.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import uk.co.suskins.commutestatus.commutestatus.models.api.CommuteStatusResponse;
 import uk.co.suskins.commutestatus.commutestatus.models.api.CommuteStatusResponseEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -13,41 +14,43 @@ import java.util.stream.Collectors;
 public class CommuteStatusMapper {
     public CommuteStatusResponse stationBoardToCommuteStatusResponse(StationBoardWithDetails stationBoardToWork,
                                                                      StationBoardWithDetails stationBoardToHome) {
-        List<CommuteStatusResponseEntity> toWork;
-        List<CommuteStatusResponseEntity> toHome;
+        List<CommuteStatusResponseEntity> toWork = new ArrayList<>();
+        List<CommuteStatusResponseEntity> toHome = new ArrayList<>();
 
-        toWork = stationBoardToWork.getTrainServices().getService().stream()
-                .map(serviceItem -> CommuteStatusResponseEntity.builder()
-                        .estimatedTimeOfDeparture(serviceItem.getEtd())
-                        .scheduledTimeOfDeparture(serviceItem.getStd())
-                        .isCancelled(
-                                (Objects.isNull(serviceItem.isIsCancelled())
-                                        ? false : serviceItem.isIsCancelled())
-                                        || (Objects.isNull(serviceItem.isFilterLocationCancelled())
-                                        ? false : serviceItem.isFilterLocationCancelled()))
-                        .platform(Objects.isNull(serviceItem.getPlatform()) ? "" : serviceItem.getPlatform())
-                        .cancelReason(Objects.isNull(serviceItem.getCancelReason()) ? "" : serviceItem.getCancelReason())
-                        .delayReason(Objects.isNull(serviceItem.getDelayReason()) ? "" : serviceItem.getDelayReason())
-                        .from(stationBoardToWork.getLocationName())
-                        .to(stationBoardToWork.getFilterLocationName())
-                        .build()).collect(Collectors.toList());
-
-        toHome = stationBoardToHome.getTrainServices().getService().stream()
-                .map(serviceItem -> CommuteStatusResponseEntity.builder()
-                        .estimatedTimeOfDeparture(serviceItem.getEtd())
-                        .scheduledTimeOfDeparture(serviceItem.getStd())
-                        .isCancelled(
-                                (Objects.isNull(serviceItem.isIsCancelled())
-                                        ? false : serviceItem.isIsCancelled())
-                                        || (Objects.isNull(serviceItem.isFilterLocationCancelled())
-                                        ? false : serviceItem.isFilterLocationCancelled()))
-                        .platform(Objects.isNull(serviceItem.getPlatform()) ? "" : serviceItem.getPlatform())
-                        .cancelReason(Objects.isNull(serviceItem.getCancelReason()) ? "" : serviceItem.getCancelReason())
-                        .delayReason(Objects.isNull(serviceItem.getDelayReason()) ? "" : serviceItem.getDelayReason())
-                        .from(stationBoardToHome.getLocationName())
-                        .to(stationBoardToHome.getFilterLocationName())
-                        .build()).collect(Collectors.toList());
-
+        if (Objects.nonNull(stationBoardToWork.getTrainServices())) {
+            toWork = stationBoardToWork.getTrainServices().getService().stream()
+                    .map(serviceItem -> CommuteStatusResponseEntity.builder()
+                            .estimatedTimeOfDeparture(serviceItem.getEtd())
+                            .scheduledTimeOfDeparture(serviceItem.getStd())
+                            .isCancelled(
+                                    (Objects.isNull(serviceItem.isIsCancelled())
+                                            ? false : serviceItem.isIsCancelled())
+                                            || (Objects.isNull(serviceItem.isFilterLocationCancelled())
+                                            ? false : serviceItem.isFilterLocationCancelled()))
+                            .platform(Objects.isNull(serviceItem.getPlatform()) ? "" : serviceItem.getPlatform())
+                            .cancelReason(Objects.isNull(serviceItem.getCancelReason()) ? "" : serviceItem.getCancelReason())
+                            .delayReason(Objects.isNull(serviceItem.getDelayReason()) ? "" : serviceItem.getDelayReason())
+                            .from(stationBoardToWork.getLocationName())
+                            .to(stationBoardToWork.getFilterLocationName())
+                            .build()).collect(Collectors.toList());
+        }
+        if (Objects.nonNull(stationBoardToHome.getTrainServices())) {
+            toHome = stationBoardToHome.getTrainServices().getService().stream()
+                    .map(serviceItem -> CommuteStatusResponseEntity.builder()
+                            .estimatedTimeOfDeparture(serviceItem.getEtd())
+                            .scheduledTimeOfDeparture(serviceItem.getStd())
+                            .isCancelled(
+                                    (Objects.isNull(serviceItem.isIsCancelled())
+                                            ? false : serviceItem.isIsCancelled())
+                                            || (Objects.isNull(serviceItem.isFilterLocationCancelled())
+                                            ? false : serviceItem.isFilterLocationCancelled()))
+                            .platform(Objects.isNull(serviceItem.getPlatform()) ? "" : serviceItem.getPlatform())
+                            .cancelReason(Objects.isNull(serviceItem.getCancelReason()) ? "" : serviceItem.getCancelReason())
+                            .delayReason(Objects.isNull(serviceItem.getDelayReason()) ? "" : serviceItem.getDelayReason())
+                            .from(stationBoardToHome.getLocationName())
+                            .to(stationBoardToHome.getFilterLocationName())
+                            .build()).collect(Collectors.toList());
+        }
         return new CommuteStatusResponse(toWork, toHome);
     }
 }
